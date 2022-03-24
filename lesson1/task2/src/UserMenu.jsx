4c7e837c4f55ef45a51bc3972240636938d92075
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-03-24 13:43:06 +0200

    redux

--- lesson1/task2/src/UserMenu.jsx
+++ lesson1/task2/src/UserMenu.jsx
@@ -1,24 +1,17 @@
-import React from "react";
+import React from 'react';
 import { UserContext } from './user-context';
 
-
 class UserMenu extends React.Component {
-   render() {
-      const { name, avatar_url } = this.context;
-      return (
-         <div class="menu">
-            <span class="menu__greeting">{name}</span>
-            <img
-               alt="User Avatar"
-               src={avatar_url}
-               class="menu__avatar"
-            />
-         </div>
-      );
-   }
+  render() {
+    const { name, avatar_url } = this.context;
+    return (
+      <div className="menu">
+        <span className="menu__greeting">{name}</span>
+        <img alt="User Avatar" src={avatar_url} className="menu__avatar" />
+      </div>
+    );
+  }
 }
-  
-
 
 UserMenu.contextType = UserContext;
 
