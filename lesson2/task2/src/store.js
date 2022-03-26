dd6bf67c654709ae5d30d4945d0960106f4ad81c
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-03-26 16:12:02 +0200

    redux

--- lesson2/task2/src/store.js
+++ lesson2/task2/src/store.js
@@ -1,46 +1,50 @@
 import { createStore } from 'redux';
 
 const INCREMENT = 'COUNTER/INCREMENT';
-const RESET = 'COUNTER/RESET';
 const DECREMENT = 'COUNTER/DECREMENT';
+const RESET = 'COUNTER/RESET';
 
 export const increment = () => {
-   return { type: INCREMENT };
-}
-
-export const reset = () => {
-   return { type: RESET };
-}
+  return { type: INCREMENT };
+};
 
 export const decrement = () => {
-   return { type: DECREMENT };
-}
+  return { type: DECREMENT };
+};
+
+export const reset = () => {
+  return { type: RESET };
+};
 
 const initialState = {
-   history: [],
-}
+  value: 0,
+  history: [],
+};
 
 const counterReducer = (state = initialState, action) => {
-   switch (action.type) {
-      case INCREMENT:
-         return {
-            ...state,
-            history: state.history.concat('+1'),
-         }
-      case DECREMENT:
-         return {
-            ...state,
-            history: state.history.concat('-1')
-         }
-      case RESET:
-         return {
-            ...state,
-            history: [],
-         }
-      default:
-         return state;
-   }
-}
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        value: state.value + 1,
+        history: state.history.concat('+1'),
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        value: state.value - 1,
+        history: state.history.concat('-1'),
+      };
+    case RESET:
+      return {
+        ...state,
+        value: 0,
+        history: [],
+      };
+    default:
+      return state;
+  }
+};
 
 const store = createStore(counterReducer);
 
