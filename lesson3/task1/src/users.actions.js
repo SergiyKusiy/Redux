312da3b807504bf26e493c24e0f29bb243237b9e
SergiyKusiy <81525113+SergiyKusiy@users.noreply.github.com>
2022-03-28 12:18:57 +0300

    redux

--- lesson3/task1/src/users.actions.js
+++ lesson3/task1/src/users.actions.js
@@ -1,31 +1,31 @@
-export const USER_ADD = 'USERS/ADD';
-export const USER_DELETE = 'USERS/USER_DELETE';
-export const USER_UPDATE = 'USERS/UPDATE';
+export const ADD_USER = 'USERS/ADD_USER';
+export const DELETE_USER = 'USERS/DELETE_USER';
+export const UPDATE_USER = 'USERS/UPDATE_USER';
 
-export const userAdd = userData => {
-   return {
-      type: USER_ADD,
-      payload: {
-         userData,
-      }
-   };
-}
+export const addUser = userData => {
+  return {
+    type: ADD_USER,
+    payload: {
+      userData,
+    },
+  };
+};
 
-export const userDelete = userId => {
-   return {
-      type: USER_DELETE,
-      payload: {
-         userId,
-      }
-   };
-}
+export const deleteUser = userId => {
+  return {
+    type: DELETE_USER,
+    payload: {
+      userId,
+    },
+  };
+};
 
-export const userUpdate = (userId, userData) => {
-   return {
-      type: USER_UPDATE,
-      payload: {
-         userId,
-         userData,
-      }
-   };
+export const updateUser = (userId, userData) => {
+  return {
+    type: UPDATE_USER,
+    payload: {
+      userId,
+      userData,
+    },
+  };
 };
